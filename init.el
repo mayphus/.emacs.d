de020faf7b149b2a9378f4aeab5cdb2b3aa1cbbe
Mayphus <tangmeifa@gmail.com>
2025-09-23 17:46:22 -0700

    Inline Org configuration

--- init.el
+++ init.el
@@ -245,6 +245,26 @@
 
 
 ;; Org, Notes
-(use-package init-org)
+(defconst my/org-notes-directory (expand-file-name "~/workspace/notes/")
+  "Primary directory for Org notes.")
+
+(use-package org
+  :ensure nil
+  :mode ("\\.org\\'" . org-mode)
+  :init
+  (unless (file-directory-p my/org-notes-directory)
+    (make-directory my/org-notes-directory t))
+  :bind (("C-c n" . org-capture)
+         ("C-c a" . org-agenda))
+  :custom
+  (org-directory my/org-notes-directory)
+  (org-default-notes-file (expand-file-name "inbox.org" my/org-notes-directory))
+  (org-agenda-files (list my/org-notes-directory))
+  (org-startup-with-inline-images t)
+  :config
+  (setq org-capture-templates
+        '(("n" "Quick note" entry
+           (file org-default-notes-file)
+           "* %?\n%U\n"))))
 
 ;;; init.el ends here
